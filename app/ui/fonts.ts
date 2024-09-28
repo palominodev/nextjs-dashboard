d05413e8241f41b5128c7ecc44a269cc3327da30
Soporte <soporte@holomedic.com.pe>
2024-09-28 10:17:17 -0500

    bugfixes deploy

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,4 +3,4 @@ import { Lusitana } from 'next/font/google';
 
 
 export const inter = Inter({subsets: ['latin']})
-export const lusiana = Lusitana({subsets: ['latin'], weight: '400'})
+export const lusitana = Lusitana({subsets: ['latin'], weight: '400'})
