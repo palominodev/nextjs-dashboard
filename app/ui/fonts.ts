c1e97f97c6698cbaa9908c813ac0fd06df85e49d
Soporte <soporte@holomedic.com.pe>
2024-09-28 10:00:07 -0500

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+
+export const inter = Inter({subsets: ['latin']})
+export const lusiana = Lusitana({subsets: ['latin'], weight: '400'})
